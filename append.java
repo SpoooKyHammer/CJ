0576c93df511e9d4ffbf68b2dda55e5270abc89c
Romulus Darwin <100368893+SpoooKyHammer@users.noreply.github.com>
2023-01-22 15:30:58 +0530

    Create append.java

---
+++ append.java
@@ -0,0 +1,10 @@
+
+//concatinate two string
+public class MyClass {
+    public static void main(String args[]) {
+      String text = "welcome";
+      String text2 = "madam";
+      String newText = text + " " + text2;
+      System.out.println(newText);
+    }
+}
