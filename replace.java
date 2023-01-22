0b4572f244ad98a478c232a7cd23f4293dd7258d
Romulus Darwin <100368893+SpoooKyHammer@users.noreply.github.com>
2023-01-22 15:27:37 +0530

    Create replace.java

---
+++ replace.java
@@ -0,0 +1,9 @@
+
+//replace "w" in "welcome" with "W"
+public class MyClass {
+    public static void main(String args[]) {
+      String text = "welcome";
+      String newText = text.replace("w","W");
+      System.out.println(newText);
+    }
+}
